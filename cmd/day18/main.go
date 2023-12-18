6a0a03448c5cbfc4e3f8ee9e553d5c5a511dbc62
Milos Zivlak <zivlakmilos@gmail.com>
2023-12-18 20:01:58 +0100

    Puzzle for day 19 part 02

--- cmd/day18/main.go
+++ cmd/day18/main.go
@@ -18,12 +18,40 @@ var dirs = map[string]Vector{
 	"R": {1, 0},
 }
 
+var dirs2 = map[byte]Vector{
+	'0': {1, 0},
+	'1': {0, 1},
+	'2': {-1, 0},
+	'3': {0, -1},
+}
+
 func abs(num int) int {
 	if num < 0 {
 		return num * -1
 	}
 
 	return num
+}
+
+func calcArea(points []Vector, totalDist int) int {
+	sum := 0
+	for i := range points {
+		prev := i - 1
+		next := i + 1
+
+		if prev < 0 {
+			prev = len(points) - 1
+		}
+		if next >= len(points) {
+			next = 0
+		}
+
+		sum += points[i].x * (points[prev].y - points[next].y)
+	}
+	a := abs(sum) / 2
+	i := a - totalDist/2 + 1
+
+	return i + totalDist
 }
 
 func solvePuzzle01() {
@@ -46,27 +74,35 @@ func solvePuzzle01() {
 		points = append(points, point)
 	}
 
-	sum := 0
-	for i := range points {
-		prev := i - 1
-		next := i + 1
+	total := calcArea(points, totalDist)
+	fmt.Printf("Total cube meters: %d\n", total)
+}
 
-		if prev < 0 {
-			prev = len(points) - 1
-		}
-		if next >= len(points) {
-			next = 0
-		}
+func solvePuzzle02() {
+	input := getInput()
+	lines := strings.Split(input, "\n")
 
-		sum += points[i].x * (points[prev].y - points[next].y)
+	points := []Vector{{0, 0}}
+
+	totalDist := 0
+	for _, line := range lines {
+		data := strings.Split(line, " ")
+		dir := dirs2[data[2][len(data[2])-2]]
+		dist, _ := strconv.ParseInt(data[2][2:len(data[2])-2], 16, 32)
+		totalDist += int(dist)
+
+		point := Vector{
+			x: points[len(points)-1].x + dir.x*int(dist),
+			y: points[len(points)-1].y + dir.y*int(dist),
+		}
+		points = append(points, point)
 	}
-	a := abs(sum) / 2
-	i := a - totalDist/2 + 1
 
-	total := i + totalDist
+	total := calcArea(points, totalDist)
 	fmt.Printf("Total cube meters: %d\n", total)
 }
 
 func main() {
 	solvePuzzle01()
+	solvePuzzle02()
 }
