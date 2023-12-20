e4e5d30b8dde826a4cc3319190169628c99bbccc
Milos Zivlak <zivlakmilos@gmail.com>
2023-12-20 19:00:00 +0100

    Puzzle for day 20 part 02

--- cmd/day20/main.go
+++ cmd/day20/main.go
@@ -141,6 +141,116 @@ func processPush(modules map[string]*Module) (int, int) {
 	return totalLow, totalHigh
 }
 
+func getRXDestModule(modules map[string]*Module) string {
+	for key, val := range modules {
+		for _, con := range val.connections {
+			if con == "rx" {
+				return key
+			}
+		}
+	}
+
+	return ""
+}
+
+func gcd(a, b int) int {
+	for a != b {
+		if a > b {
+			a -= b
+		} else {
+			b -= a
+		}
+	}
+
+	return a
+}
+
+func countUntilRX(modules map[string]*Module) int {
+	feed := getRXDestModule(modules)
+
+	cycleLenghts := map[string]int{}
+	seen := map[string]int{}
+
+	for key, val := range modules {
+		for _, con := range val.connections {
+			if con == feed {
+				seen[key] = 0
+			}
+		}
+	}
+
+	presses := 0
+	for {
+		presses++
+
+		modules["broadcaster"].value = 0
+		queue := Queue{QueueData{src: "button", dst: "broadcaster", value: 0}}
+
+		for len(queue) > 0 {
+			pulse := queue.Pop()
+
+			module, ok := modules[pulse.dst]
+			if !ok {
+				continue
+			}
+
+			if pulse.dst == feed && pulse.value == 1 {
+				seen[pulse.src]++
+				if _, ok := cycleLenghts[pulse.src]; !ok {
+					cycleLenghts[pulse.src] = presses
+				}
+
+				seenAll := true
+				for _, s := range seen {
+					if s == 0 {
+						seenAll = false
+						break
+					}
+				}
+
+				if seenAll {
+					x := 1
+					for _, cycleLen := range cycleLenghts {
+						x *= cycleLen / gcd(x, cycleLen)
+					}
+					return x
+				}
+			}
+
+			sends := true
+			switch module.moduleType {
+			case ModuleTypeFlipFlop:
+				if pulse.value > 0 {
+					sends = false
+				} else {
+					if module.value > 0 {
+						module.value = 0
+					} else {
+						module.value = 1
+					}
+				}
+			case ModuleTypeConjuction:
+				module.state[pulse.src] = pulse.value
+				module.value = 0
+				for _, s := range module.state {
+					if s == 0 {
+						module.value = 1
+						break
+					}
+				}
+			default:
+				module.value = pulse.value
+			}
+
+			if sends {
+				for _, con := range module.connections {
+					queue.Add(QueueData{src: pulse.dst, dst: con, value: module.value})
+				}
+			}
+		}
+	}
+}
+
 func solvePuzzle01() {
 	input := getInput()
 
@@ -157,6 +267,16 @@ func solvePuzzle01() {
 	fmt.Printf("Total pulses: %d\n", lo*hi)
 }
 
+func solvePuzzle02() {
+	input := getInput()
+
+	modules := parseInput(input)
+
+	presses := countUntilRX(modules)
+	fmt.Printf("Button presses: %d\n", presses)
+}
+
 func main() {
 	solvePuzzle01()
+	solvePuzzle02()
 }
