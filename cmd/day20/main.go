e9100b9b4e52f1f556984a3ef9a9105fec022f36
Milos Zivlak <zivlakmilos@gmail.com>
2023-12-20 18:00:00 +0100

    Puzzle for day 20 part 01

---
+++ cmd/day20/main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type ModuleType int
+
+const (
+	ModuleTypeBroadcaster ModuleType = iota
+	ModuleTypeFlipFlop
+	ModuleTypeConjuction
+)
+
+type Module struct {
+	state       map[string]int
+	connections []string
+	moduleType  ModuleType
+	value       int
+}
+
+func (m *Module) String() string {
+	return fmt.Sprintf("{ type: %d, value: %d, connections: %s}\n", m.moduleType, m.value, m.connections)
+}
+
+type QueueData struct {
+	src   string
+	dst   string
+	value int
+}
+
+type Queue []QueueData
+
+func (q *Queue) Pop() QueueData {
+	item := (*q)[0]
+	(*q) = (*q)[1:]
+	return item
+}
+
+func (q *Queue) Add(data QueueData) {
+	*q = append(*q, data)
+}
+
+func parseInput(input string) map[string]*Module {
+	res := map[string]*Module{}
+
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		data := strings.Split(line, " -> ")
+		connections := strings.Split(data[1], ", ")
+
+		key := data[0]
+		moduleType := ModuleTypeBroadcaster
+		state := map[string]int{}
+
+		if data[0] != "broadcaster" {
+			switch data[0][0] {
+			case '%':
+				moduleType = ModuleTypeFlipFlop
+			case '&':
+				moduleType = ModuleTypeConjuction
+			}
+			key = key[1:]
+		}
+
+		module := &Module{
+			moduleType:  moduleType,
+			connections: connections,
+			value:       0,
+			state:       state,
+		}
+		res[key] = module
+	}
+
+	for key, val := range res {
+		for _, con := range val.connections {
+			if _, ok := res[con]; !ok {
+				continue
+			}
+			res[con].state[key] = 0
+		}
+	}
+
+	return res
+}
+
+func processPush(modules map[string]*Module) (int, int) {
+	totalLow := 0
+	totalHigh := 0
+
+	modules["broadcaster"].value = 0
+	queue := Queue{QueueData{src: "button", dst: "broadcaster", value: 0}}
+
+	for len(queue) > 0 {
+		pulse := queue.Pop()
+
+		if pulse.value > 0 {
+			totalHigh++
+		} else {
+			totalLow++
+		}
+
+		module, ok := modules[pulse.dst]
+		if !ok {
+			continue
+		}
+
+		sends := true
+		switch module.moduleType {
+		case ModuleTypeFlipFlop:
+			if pulse.value > 0 {
+				sends = false
+			} else {
+				if module.value > 0 {
+					module.value = 0
+				} else {
+					module.value = 1
+				}
+			}
+		case ModuleTypeConjuction:
+			module.state[pulse.src] = pulse.value
+			module.value = 0
+			for _, s := range module.state {
+				if s == 0 {
+					module.value = 1
+					break
+				}
+			}
+		default:
+			module.value = pulse.value
+		}
+
+		if sends {
+			for _, con := range module.connections {
+				queue.Add(QueueData{src: pulse.dst, dst: con, value: module.value})
+			}
+		}
+	}
+
+	return totalLow, totalHigh
+}
+
+func solvePuzzle01() {
+	input := getInput()
+
+	modules := parseInput(input)
+
+	lo := 0
+	hi := 0
+	for i := 0; i < 1000; i++ {
+		l, h := processPush(modules)
+		lo += l
+		hi += h
+	}
+
+	fmt.Printf("Total pulses: %d\n", lo*hi)
+}
+
+func main() {
+	solvePuzzle01()
+}
